294830c51fbb65908092b068d5a7fa151d394004
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-06 12:16:16 -0800

    js practice part 1

---
+++ index.js
@@ -0,0 +1,26 @@
+// parameters and arguments- the way i remember this is P.A
+
+// keywords are automatically recognized by the code at run time
+
+// var something = "hello world"; 
+
+// console.log(something)
+
+// function cookies(){
+//     console.log("1234567890")
+//     console.log(1234567890)
+
+// }
+// cookies();
+var args = "hello Bananas"
+var pizza = "mama mia!!!"
+var nums = 12345
+function bananas(params){
+    
+    console.log(params)
+}
+
+bananas(pizza)
+bananas (args)
+bananas (nums)
+
